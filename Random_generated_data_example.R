42e31ad606d4cbcacf4525d6a924d4326ff0f7b7
craigpoku <65303787+craigpoku@users.noreply.github.com>
2022-06-10 18:17:04 +0100

    Updated code to be more streamline and also do a 3 week moving window

--- Random_generated_data_example.R
+++ Random_generated_data_example.R
@@ -17,20 +17,20 @@ fun_2_df = linear_function_generator(x, g_x, tail(x, n=1), 0.01)
 plot(fun_2_df, type = "l")
 
 
-win_length_vector = c(7)
+win_length_vector = c(10, 60, 250)
 
 fun_1_cp_detection = map_dfr(.x = win_length_vector,
                              .f = ~window_length_constrain(df = fun_1_df,
                                                            .x, cp_factor = 1e2,
                                                            epsilon = 1e-9, date = FALSE))
+theme_set(theme_gray(base_size = 18))
 
-fun_2_cp_detection = map_dfr(.x = win_length_vector,
-                             .f = ~window_length_constrain(df = fun_2_df,
-                                                           .x, cp_factor = 1e2,
-                                                           epsilon = 1e-9, date = FALSE))
-
-
-
-
-
-
+fun_1_cp_detection %>%
+  filter(variables != "r.squareds") %>%
+  ggplot(aes(x = index, y = value)) +
+  geom_line(aes(colour=variables), lwd = 1.5)+
+  geom_vline(data = filter(fun_1_cp_detection,
+                           cp==TRUE, variables == "Test dataset"),
+             aes(xintercept = index), size  = 1, colour = "blue")+
+  labs(x= "Date", y = "Various Units", colour = "Variables")+
+  facet_grid(vars(variables), vars(window_length_level), scales = "free_y")
--- ulez_cpd_applicaton_visualisation_example.R
+++ ulez_cpd_applicaton_visualisation_example.R
@@ -6,7 +6,7 @@
 #reads in compliance data based on FOI source: 
 #https://tfl.gov.uk/corporate/transparency/freedom-of-information/foi-request-detail?referenceId=FOI-2045-1920
 
-ULEZ_compliance_df = read.csv("~/GitHub/York_WACL/ULEZ_cp_project/ULEZVehiclesCharges_PCs.csv") %>%
+ULEZ_compliance_df = read.csv("~/Github/live_change_point_development/ULEZVehiclesCharges_PCs.csv") %>%
   rename(date = "Date.of.Travel",
          total_vehicle_number = "Unique.VRMs.Captured.in.Charging.Hours",
          compliant_number = "Unique.VRMs.Compliant.with.ULEZ",
@@ -20,8 +20,9 @@ ULEZ_compliance_df = read.csv("~/GitHub/York_WACL/ULEZ_cp_project/ULEZVehiclesCh
          non_compliant_number = as.numeric(gsub(",","",non_compliant_number)),
          compliant_percentage = (compliant_number/total_vehicle_number)*100.,
          d7_rollavg_compliant_percentage = roll_mean(compliant_percentage, n = 7, align = "right", 
-                                               fill = NA))%>%
+                                                     fill = NA))%>%
   drop_na()
+
 
 
 win_length_vector = c(7)
@@ -30,70 +31,94 @@ win_length_vector = c(7)
 
 cp_urban_background_no2_df_all = normalised_urban_background_no2_all_sites_reformat %>%
   filter(date >= as.Date("2019-01-01") & date <= as.Date("2019-06-30")) %>%
-  select(date, d7_rollavg_normal_mean)%>%
+  select(date, normal_mean)%>%
   mutate(df_header = "Urban background NO2") %>%
-  rename(value = d7_rollavg_normal_mean)
+  rename(value = normal_mean)
 
 cp_roadside_no2_df_all = normalised_roadside_no2_all_sites_reformat %>%
   filter(date >= as.Date("2019-01-01") & date <= as.Date("2019-06-30")) %>%
-  select(date, d7_rollavg_normal_mean)%>%
+  select(date, normal_mean)%>%
   mutate(df_header = "Roadside NO2") %>%
-  rename(value = d7_rollavg_normal_mean)
+  rename(value = normal_mean)
 
 cp_roadside_pm25_df_ULEZ = normalised_roadside_pm25_ULEZ_reformat %>%
-  filter(date >= as.Date("2019-01-01") & date <= as.Date("2019-06-30")) %>%
-  select(date, d7_rollavg_normal_mean)%>%
+  filter(date >= as.Date("2019-01-01") & date <= as.Date("2019-07-30")) %>%
+  select(date, normal_mean)%>%
   mutate(df_header = "Roadside PM2.5") %>%
-  rename(value = d7_rollavg_normal_mean)
+  rename(value = normal_mean)
 
 cp_roadside_no2_df_ULEZ = normalised_roadside_no2_ULEZ_reformat %>%
-  filter(date >= as.Date("2019-01-01") & date <= as.Date("2019-06-30")) %>%
-  select(date, d7_rollavg_normal_mean)%>%
+  filter(date >= as.Date("2019-01-01") & date <= as.Date("2019-07-30")) %>%
+  select(date, normal_mean)%>%
   mutate(df_header = "Roadside NO2") %>%
-  rename(value = d7_rollavg_normal_mean)
+  rename(value = normal_mean)
 
 compliance_statistics_cp_df = ULEZ_compliance_df  %>%
   filter(date >= as.Date("2019-01-01") & date <= as.Date("2019-06-30")) %>%
-  select(date, d7_rollavg_compliant_percentage) %>%
-  mutate(df_header = "Compiance stats") %>%
-  rename(value = d7_rollavg_compliant_percentage)
+  select(date, total_vehicle_number) %>%
+  mutate(df_header = "Traffic count") %>%
+  rename(value = total_vehicle_number)
 
 #Creates multivariant TS df
 
-ULEZ_total_cp_df = rbind(cp_roadside_no2_df_ULEZ,
-                         cp_roadside_pm25_df_ULEZ,
-                         compliance_statistics_cp_df)
+ULEZ_total_cp_df = rbind(cp_roadside_no2_df_ULEZ)
 
 ULEZ_total_cp_code = unique(as.character(ULEZ_total_cp_df$df_header))
 
 #applies CP to dataframe, in this case ULEZ
 
-ULEZ_example_detected_cps = map2_dfr(.x = 7, .y = ULEZ_total_cp_code,
+ULEZ_example_detected_cps = map2_dfr(.x = 25, .y = ULEZ_total_cp_code,
                   .f = ~multi_var_ts_gradient_cp_detection(df = ULEZ_total_cp_df,
-                                                           .x, .y, cp_factor = 2,
+                                                           .x, .y, cp_factor = 2.5,
                                                            epsilon = 1e-9, date = TRUE))
+
+test = change_point_model_statistics(ULEZ_example_detected_cps, 25, FALSE)
 
 
 ULEZ_example_coinciding_CPs = coinciding_cp_generator(ULEZ_example_detected_cps)
 
+theme_set(theme_gray(base_size = 20))
+
 ULEZ_example_detected_cps %>%
-  filter(date >= as.Date("2019-03-01") & date <= as.Date("2019-06-30"), 
+  filter(date >= as.Date("2019-04-01") & date <= as.Date("2019-06-30"),
+         variables != "r.squareds") %>%
+  ggplot(aes(x = date, y = value)) +
+  geom_line(aes(colour=variables), lwd = 1.5)+
+  geom_vline(data = filter(ULEZ_example_detected_cps,
+                           cp==TRUE, date >= as.POSIXct("2019-04-01"),
+                           variables != "r.squareds"),
+             aes(xintercept = date), size  = 1, colour = "blue")+
+  labs(x= "Date", y = "Various Units", colour = "Variables")+
+  facet_grid(vars(variables), vars(window_length_level), scales = "free_y")
+
+ULEZ_example_detected_cps %>%
+  filter(date >= as.Date("2019-04-01") & date <= as.Date("2019-07-30"), 
          variables == "Input dataset") %>% 
   ggplot(aes(x = date, y = value)) + 
-  geom_line(aes(colour=df_label), lwd = 1.5)+
+  geom_line(aes(colour=df_label), lwd = 2)+
+  geom_vline(data = filter(ULEZ_example_detected_cps,
+                           cp==TRUE & date >= as.POSIXct("2019-04-01") & variables %in% c("Input dataset")),
+             aes(xintercept = date), colour = "blue", size = 1.2)+
+  geom_vline(data = filter(ULEZ_example_detected_cps,
+                           cp==TRUE & date >= as.POSIXct("2019-04-01") & variables %in% c("Input dataset")
+                           & date %in% ULEZ_example_coinciding_CPs),
+             aes(xintercept = date), colour = "red", size = 1.2)+ 
+  geom_line(data = filter(test, date >= as.Date("2019-04-01") & date <= as.Date("2019-07-30"),
+                          variables == "Approxed f(x)"), colour = "green", 
+            linetype="dashed", lwd = 1.5)+
   annotate("rect", xmin = as.POSIXct("2019-04-08"), 
-           xmax = as.POSIXct("2019-06-30"), ymin = -Inf, ymax = Inf, 
+           xmax = as.POSIXct("2019-07-30"), ymin = -Inf, ymax = Inf, 
            alpha = .2)+
-  geom_point(data = filter(ULEZ_example_detected_cps,
-                           cp==TRUE & date >= as.POSIXct("2019-03-15") & variables %in% c("Input dataset"))
-             , size  = 3, colour = "blue")+
-  geom_point(data = filter(ULEZ_example_detected_cps,
-                           cp==TRUE & date >= as.POSIXct("2019-03-15") & variables %in% c("Input dataset")
-                           & date %in% ULEZ_example_coinciding_CPs)
-             , size  = 4, colour = "red")+
-  labs(x= "Date", y = "Rolling 7 day Various Units", colour = "ULEZ data sources")+
+  labs(x= "Date", colour = "ULEZ data sources")+ylab(quickText("Normalised NO2 (ug/m3)"))+
   facet_grid(df_label~., scales = "free_y")+
-  ggtitle("Applied CPD example - ULEZ AQ vs compliance")
+  ggtitle("Applied CPD example - ULEZ")
 
-  
+
+test %>%
+  filter(date >= as.Date("2019-04-01") & date <= as.Date("2019-07-30")) %>% 
+  ggplot(aes(x = date, y = value)) + 
+  geom_line(aes(colour=variables), lwd = 2)+
+  labs(x= "Date", colour = "ULEZ data sources")+ylab(quickText("Normalised pollutants (ug/m3)"))+
+  ggtitle("Applied CPD example - ULEZ")
+
 
