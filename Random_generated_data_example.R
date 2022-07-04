162d356334df11ebe5a0c131abdc8b24352d90b2
craigpoku <65303787+craigpoku@users.noreply.github.com>
2022-07-04 11:29:33 +0100

    FIxing conflict

--- Random_generated_data_example.R
+++ Random_generated_data_example.R
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 #example script that was used to test CPD debugging
 
 x = c(0, 300, 600, 900, 1200, 1500)
@@ -63,3 +64,62 @@ fun_1_cp_detection %>%
   ggtitle(paste("Standard deviation from 2nd derivative mean:",
                 as.character(sd_v), ",", as.character(per_confidence), "%"))
 
+=======
+  
+  #example script that was used to test CPD debugging
+  
+  x = c(0, 300, 600, 900, 1200, 1500)
+  f_x = c(-5, 8, 1, 2, 3, 6)
+  g_x = c(-5, 8, 1, 2, 3, 4.5)
+  
+  
+  
+  plot(x, f_x, type = "l")
+  plot(x, g_x, type = "l")
+  
+  
+  fun_1_df = linear_function_generator(x, f_x, tail(x, n=1), 0.01) %>%
+    mutate(df_header = "test")
+  fun_2_df = linear_function_generator(x, g_x, tail(x, n=1), 0.01)
+  
+  plot(fun_2_df, type = "l")
+  
+  
+  win_length_vector = c(10, 60, 250)
+  
+  fun_1_cp_detection = map_dfr(.x = win_length_vector,
+                               .f = ~multi_var_ts_gradient_cp_detection(df = fun_1_df,
+                                                             .x, "test", cp_factor = 1e2,
+                                                             epsilon = 1e-9, date = FALSE))
+  fun_1_cp_detection_example = map_dfr(.x = 60,
+                               .f = ~window_length_constrain(df = fun_1_df,
+                                                             .x, cp_factor = 1e2,
+                                                             epsilon = 1e-9, date = FALSE))
+  
+  
+  theme_set(theme_gray(base_size = 18))
+  
+  fun_1_cp_detection_example %>%
+    filter(variables == "Test dataset") %>%
+    ggplot(aes(x = index, y = value)) +
+    geom_line(colour = "red", lwd = 3)+
+    labs(x= "x", y = "f(x)")+
+    geom_vline(data = filter(fun_1_cp_detection_example,
+                             cp==TRUE, variables == "Test dataset"),
+               aes(xintercept = index), size  = 1.2, colour = "blue")+
+    labs(x= "Date", y = "Various Units", colour = "Variables")+
+    facet_grid(vars(variables), vars(window_length_level), scales = "free_y")
+  
+  
+  
+  
+  fun_1_cp_detection %>%
+    filter(variables %in% c("Test dataset", "2nd derivative", "cp_ratio")) %>%
+    ggplot(aes(x = index, y = value)) +
+    geom_line(aes(colour=variables), lwd = 1.5)+
+    geom_vline(data = filter(fun_1_cp_detection,
+                             cp==TRUE, variables == "Test dataset"),
+               aes(xintercept = index), size  = 1, colour = "blue")+
+    labs(x= "x", y = "Various Units", colour = "Variables")+
+    facet_grid(vars(variables), vars(window_length_level), scales = "free_y")
+>>>>>>> Stashed changes
